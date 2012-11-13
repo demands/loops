e4a6d2a0e209b4749418ae988172fcbac64bbe20
Max Edmands <me@maxedmands.com>
2012-11-13 09:04:15 -0800

    Modify logger to use simple IO instead of Ruby's stdlib ::Logger class

--- lib/loops/logger.rb
+++ lib/loops/logger.rb
@@ -23,17 +23,11 @@ class Loops::Logger < ::Delegator
   # @param [Integer] level
   #   Logging level. Constants are defined in +Logger+ namespace: +DEBUG+, +INFO+,
   #   +WARN+, +ERROR+, +FATAL+, or +UNKNOWN+.
-  # @param [Integer] number_of_files
-  #   A number of files to keep.
-  # @param [Integer] max_file_size
-  #   A max file size. When file become larger, next one will be created.
   # @param [Boolean] write_to_console
   #   When +true+, all logging output will be dumped to the +STDOUT+ also.
   #
-  def initialize(logfile = $stdout, level = ::Logger::INFO, number_of_files = 10, max_file_size = 100 * 1024 * 1024,
-                 write_to_console = false)
-    @number_of_files, @level, @max_file_size, @write_to_console =
-        number_of_files, level, max_file_size, write_to_console
+  def initialize(logfile = $stdout, level = :info, write_to_console = false)
+    @write_to_console = write_to_console
     self.logfile = logfile
     super(@implementation)
   end
@@ -78,7 +72,7 @@ class Loops::Logger < ::Delegator
     FileUtils.mkdir_p(File.dirname(coerced_logfile)) if String === coerced_logfile
 
     # Create a logger implementation.
-    @implementation = LoggerImplementation.new(coerced_logfile, @number_of_files, @max_file_size, @write_to_console, @colorful_logs)
+    @implementation = LoggerImplementation.new(coerced_logfile, @write_to_console, @colorful_logs)
     @implementation.level = @level
     logfile
   end
@@ -145,14 +139,13 @@ class Loops::Logger < ::Delegator
   end
 
   # @private
-  class LoggerImplementation < ::Logger
+  class LoggerImplementation
 
     attr_reader :prefix
 
     attr_accessor :write_to_console, :colorful_logs
 
     class Formatter
-
       def initialize(logger)
         @logger = logger
       end
@@ -166,37 +159,33 @@ class Loops::Logger < ::Delegator
       end
     end
 
-    def initialize(log_device, number_of_files = 10, max_file_size = 10 * 1024 * 1024, write_to_console = true, colorful_logs = false)
+    def initialize(log_device, write_to_console = true, colorful_logs = false)
       super(log_device, number_of_files, max_file_size)
-      self.formatter    = Formatter.new(self)
-      @write_to_console = write_to_console
-      @colorful_logs    = colorful_logs
-      @prefix           = nil
+      @log_device_descriptor = log_device
+      @log_device            = IO.new(log_device)
+      @formatter             = Formatter.new(self)
+      @write_to_console      = write_to_console
+      @colorful_logs         = colorful_logs
+      @prefix                = nil
     end
 
-    def add(severity, message = nil, progname = nil, &block)
-      begin
-        if @colorful_logs
-          message = color_errors(severity, message)
-          progname = color_errors(severity, progname)
+    [ :debug, :error, :fatal, :info, :warn ].each do |severity|
+      class_eval <<-EVAL, __FILE__, __LINE__
+        def #{severity}(message)
+          add(severity, message)
         end
-        super(severity, message, progname, &block)
-        if @write_to_console && (message || progname)
-          puts self.formatter.call(%w(D I W E F A)[severity] || 'A', Time.now, progname, message)
+      EVAL
+    end
+
+    def add(severity, message = nil)
+      begin
+        message = color_errors(severity, message) if @colorful_logs
+        @log_device.puts(message)
+        if @write_to_console && message
+          puts @formatter.call(%w(D I W E F A)[severity] || 'A', Time.now, progname, message)
         end
       rescue
         # ignore errors in logging
-      end
-    end
-
-    def with_prefix(prefix)
-      raise "No block given" unless block_given?
-      old_prefix = @prefix
-      @prefix = prefix
-      begin
-        yield
-      ensure
-        @prefix = old_prefix
       end
     end
 
